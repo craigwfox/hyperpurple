8fca7cc407ee2a751ae443d1b742d5bfac0d74bd
Craig Fox <craig@craigwfox.com>
2017-10-03 08:38:08 -0500

    changed green

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const foregroundColor = '#dddddd';
 
 // Colors
 const RED = '#d1267f';
-const GREEN = '#19ad16';
+const GREEN = '#2ce07d';
 const YELLOW = '#e0de60';
 const BLUE = '#906ee5';
 const MAGENTA = '#c5257a';
